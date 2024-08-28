251379312c926791c4c81a8e18bceee27452cc1f
Mehedi027 <mehedi.shuvo027@gmail.com>
2024-08-28 22:15:17 +0200

    initiate comit

--- if_else_Conditions.py
+++ if_else_Conditions.py
@@ -4,15 +4,22 @@ bill = 0
 if height >= 120:
     print("You can ride the rollercoster")
     Age = int(input("What is your age?"))
-    if Age > 18:
-        bill = 12
-        print("Please pay 12 Euro")
-    elif Age < 12:
+
+    if Age < 12:
         bill = 5
         print("Please pay 5 Euro")
-    else:
+    elif Age >= 12 and Age <= 18:
         bill = 7
         print("Please pay 7 Euro")
+    else:
+            
+        if Age <= 45  and Age <= 55:
+            bill = 0
+            print("Thank you for your visit you do not have to pay")
+        else:
+            bill = 12
+            print("Please pay 12 Euro")
+
     Photo = input("Do you need a photo during ride?")
     if Photo == "yes":
        print("Your total bill is:", bill + 3, "Euro")
